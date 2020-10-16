9c5df8dff3245de5fdf7f5008bf0907c33cf9d8e
MisakaNetwork <2269656714g@gmail.com>
2020-10-16 17:34:11 +0800

    初始化项目

---
+++ src/main/java/com/mn/level6shift/Level6ShiftApplication.java
@@ -0,0 +1,13 @@
+package com.mn.level6shift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Level6ShiftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Level6ShiftApplication.class, args);
+    }
+
+}
