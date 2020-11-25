a8c37585600b09295a93296df3e56fed4fc0ca91
MisakaNetwork <2269656714g@gmail.com>
2020-11-25 11:09:17 +0800

    feat:Application中增加EnableConfigurationProperties注解

--- src/main/java/com/mn/level6shift/Level6ShiftApplication.java
+++ src/main/java/com/mn/level6shift/Level6ShiftApplication.java
@@ -2,8 +2,10 @@ package com.mn.level6shift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class Level6ShiftApplication {
 
     public static void main(String[] args) {
